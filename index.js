56350c650216397fcc480d0f6d339c1e162c07a7
Dominic <dd429@njit.edu>
2019-10-23 15:33:45 -0400

    Created routes to view campgrounds, list campgrounds, add campground. Created new folder to hold models

--- index.js
+++ index.js
@@ -1,43 +1,50 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var app = express()
-var port = 4000
+var express     = require('express'),
+    bodyParser  = require('body-parser'),
+    mongoose    = require('mongoose'),
+    app         = express(),
+    Campground  = require("./models/campground")
+    port        = 4000
 
+mongoose.connect("mongodb://localhost/yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true})
 app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended: true}))
-
-var campgrounds = [
-    {name: "Niagara Falls", image: "https://upload.wikimedia.org/wikipedia/commons/a/ab/3Falls_Niagara.jpg"},
-    {name: "Marine Drive", image: "https://www.tripsavvy.com/thmb/yUM8q2-VUym6o52vUX8DkKlq4vk=/4241x2442/filters:fill(auto,1)/GettyImages-1008831236-5c65d6bf4cedfd00014aa310.jpg"},
-    {name: "Sea-link", image: "https://cdn.dnaindia.com/sites/default/files/styles/full/public/2017/07/03/589680-sea-link-bandra-worli-070317.jpg"},
-    {name: "Niagara Falls", image: "https://upload.wikimedia.org/wikipedia/commons/a/ab/3Falls_Niagara.jpg"},
-    {name: "Marine Drive", image: "https://www.tripsavvy.com/thmb/yUM8q2-VUym6o52vUX8DkKlq4vk=/4241x2442/filters:fill(auto,1)/GettyImages-1008831236-5c65d6bf4cedfd00014aa310.jpg"},
-    {name: "Sea-link", image: "https://cdn.dnaindia.com/sites/default/files/styles/full/public/2017/07/03/589680-sea-link-bandra-worli-070317.jpg"},
-    {name: "Niagara Falls", image: "https://upload.wikimedia.org/wikipedia/commons/a/ab/3Falls_Niagara.jpg"},
-    {name: "Marine Drive", image: "https://www.tripsavvy.com/thmb/yUM8q2-VUym6o52vUX8DkKlq4vk=/4241x2442/filters:fill(auto,1)/GettyImages-1008831236-5c65d6bf4cedfd00014aa310.jpg"},
-    {name: "Sea-link", image: "https://cdn.dnaindia.com/sites/default/files/styles/full/public/2017/07/03/589680-sea-link-bandra-worli-070317.jpg"}
-]
 
 app.get("/", function(req, res){
     res.render("landing")
 })
 
+// adding a new campground page
 app.get("/campgrounds/new", function(req, res){
     res.render("new")
 })
 
+// viewing all campgrounds
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds",{campgrounds: campgrounds})
+    Campground.find({}, function(err, campgrounds){
+        if(err) console.log(err)
+        else res.render("index", {campgrounds: campgrounds})
+    })
 })
 
+// inserting a new campground
 app.post("/campgrounds", function(req, res){
     name = req.body.name;
     image = req.body.image;
-    var newCampGround = {name: name, image: image}
-    campgrounds.push(newCampGround)
-    res.redirect("campgrounds")
-    // get data from the campgrounds array
-    // redirect back to campgrounds page
+    description = req.body.description;
+    var newCampGround = {name: name, image: image, description: description}
+    Campground.create(newCampGround, function(err, newCampground){
+        if(err) console.log(err)
+        else res.redirect("campgrounds")
+    })
+})
+
+// show information of a campground
+app.get("/campgrounds/:id", function(req, res){
+    var id = req.params.id
+    Campground.findById(id, function(err, foundCampground){
+        if(err) console.log(err)
+        else res.render("show", {campground: foundCampground})
+    })
 })
 
 app.listen(port, function(){
